103c7907efbef1b185733280efecd2ec92011f4a
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-09-19 21:34:51 +0300

    Added support for mypy and flake8

--- CodeReviewBot/CodeReviewBot/urls.py
+++ CodeReviewBot/CodeReviewBot/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.auth.models import User
+from django.urls import include, path
 from rest_framework import routers, serializers, viewsets
 
 
@@ -8,12 +8,12 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
-        
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
